e04a1ce24796c04e01c4e93240af5672e6d61d61
miguel <miguelpayet@gmail.com>
2019-08-02 20:59:29 -0500

    contacto + canonical

--- app/my_context_processors.py
+++ app/my_context_processors.py
@@ -4,5 +4,6 @@ from django.conf import settings
 def settings_context_processor(request):
     settings_dict = {
         'MEDIA_URL': settings.MEDIA_URL,
+        'CANONICAL_PATH': request.build_absolute_uri(request.path)
     }
     return settings_dict
